7db7de80e0159c59bae621d5e04e5b3a99c214e2
irsyad <irsyad@sensorflow.org>
2019-04-28 03:04:09 +0800

    get react-spring running

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {useSpring, animated, config} from 'react-spring'
+import { ReactComponent as RocketLogo} from './Falcon9.svg';
 import './App.css';
 
 function App() {
+  const AnimatedRocket = animated(RocketLogo)
+  const [isLanded, setLanded] = useState(false)
+  const props = useSpring({
+    transform: 'translateY(0)',
+    from: {transform: 'translateY(-20em)'},
+    reset: !isLanded,
+    reverse: isLanded,
+    config: { duration: 3000}})
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AnimatedRocket style={props} className="App-logo"/>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={() => setLanded(!isLanded)}>{isLanded ? 'Land' : 'Launch'}</button>
         <a
           className="App-link"
           href="https://reactjs.org"
